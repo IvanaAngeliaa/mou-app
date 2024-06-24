3d3e88058df54c1ecb5e25b0a23b186de5b82117
eceqq <124455131+eceqq@users.noreply.github.com>
2024-06-24 23:27:26 +0700

    update images calling

--- api/assets.php
+++ api/assets.php
@@ -10,4 +10,7 @@ if ($_GET['type'] === 'css') {
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
     echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+}  else if ($_GET['type'] === 'images') {
+    header('Content-Type: application/javascript; charset: UTF-8');
+    echo require __DIR__ . '/../public/images/' . basename($_GET['file']);
 }
